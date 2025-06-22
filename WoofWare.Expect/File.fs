9c7874098665b3c2fe3c4704721022bdb2e03c3b
Smaug123 <3138005+Smaug123@users.noreply.github.com>
2025-06-22 23:02:20 +0100

    Better

--- WoofWare.Expect/File.fs
+++ WoofWare.Expect/File.fs
@@ -6,14 +6,25 @@ open System.IO
 [<RequireQualifiedAccess>]
 module internal File =
 
-    /// Standard attempt at an atomic file write
+    /// Standard attempt at an atomic file write.
+    /// It may fail to be atomic if the working directory somehow spans multiple volumes,
+    /// and of course with external network storage all bets are off.
     let writeAllLines (lines : string[]) (path : string) : unit =
         let file = FileInfo path
-        let tempFile = Path.Combine (file.Directory.FullName, Guid.NewGuid().ToString ())
+
+        let tempFile =
+            Path.Combine (file.Directory.FullName, file.Name + "." + Guid.NewGuid().ToString () + ".tmp")
 
         try
             File.WriteAllLines (tempFile, lines)
-            File.Move (tempFile, path)
+            // Atomicity guarantees are undocumented, but on Unix this is an atomic `rename` call
+            // https://github.com/dotnet/runtime/blob/9a4be5b56d81aa04c7ea687c02b3f4e64c83761b/src/libraries/System.Private.CoreLib/src/System/IO/FileSystem.Unix.cs#L181
+            // and on Windows this is an atomic ReplaceFile:
+            // https://github.com/dotnet/runtime/blob/9a4be5b56d81aa04c7ea687c02b3f4e64c83761b/src/libraries/System.Private.CoreLib/src/System/IO/FileSystem.Windows.cs#L92
+            // calls https://github.com/dotnet/runtime/blob/9a4be5b56d81aa04c7ea687c02b3f4e64c83761b/src/libraries/Common/src/Interop/Windows/Kernel32/Interop.ReplaceFile.cs#L12
+            // which calls ReplaceFileW, whose atomicity guarantees are again apparently undocumented,
+            // but 4o-turbo, Opus 4, and Gemini 2.5 Flash all think it's atomic.
+            File.Replace (tempFile, path, null)
         finally
             try
                 File.Delete tempFile
